21d159ceb2ed2423ccb374c8e272e7ec17586726
Ona0nline <onasihle123@gmail.com>
2024-09-05 14:48:51 +0200

    Update main.py

--- Challenge1/main.py
+++ Challenge1/main.py
@@ -4,5 +4,7 @@ with open("Challenge1/data.csv","r") as file:
   csv_reader = csv.DictReader(file)
   
   for line in csv_reader:
+    
+    """Different syntax with csv files (dictionaries)"""
     print(f"In {line['year']} there were {line['population']} people in {line['continent']}")
     # print(f"In {line[1]}, there were {line[2]} people in {line[0]}. ")
